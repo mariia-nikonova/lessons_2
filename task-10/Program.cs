cd6695cb72b5af8eebe56fe519350e2f80e57af1
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-07-24 21:22:00 +0300

    task-10_commit

---
+++ task-10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трехзначное число: ");
+int numb = int.Parse(Console.ReadLine()!);
+
+if (numb >= 100 && numb < 1000)
+{
+    int second = (numb%100-numb%10)/10;
+    System.Console.WriteLine($"Вторая цифра числа {numb} - {second}");
+}
+else
+    System.Console.WriteLine("Число не является трехзначным");
