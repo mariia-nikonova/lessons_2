e922c6c0a3b38c026339c0548e4d37536001fe14
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-07-28 09:14:48 +0300

    task-13_commit

---
+++ task-13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int numb = int.Parse(Console.ReadLine()!);
+int tmp = numb;
+
+if (numb >= 100)
+{
+    while (numb >= 1000)
+    {
+        numb = numb / 10;
+    }
+    int third = numb % 10;
+    System.Console.WriteLine($"{tmp} -> {third}");
+}
+else System.Console.WriteLine("Третьей цифры нет");
