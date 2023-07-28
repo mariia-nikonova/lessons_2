9b26a6b6689bbeabffa5967feae2a8e0ff9cef24
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-07-28 09:22:54 +0300

    task-15_commit

---
+++ task-15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int numb = int.Parse(Console.ReadLine()!);
+
+if ((numb == 7) || (numb == 6)) Console.WriteLine($"{numb} - да");
+else Console.WriteLine($"{numb} - нет");
